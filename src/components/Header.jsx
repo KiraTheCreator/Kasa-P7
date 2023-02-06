ad27c89a4e360597e9e812c7e136e2c6259b04a5
Som Thea <theadeveloppeurweb@gmail.com>
2023-02-06 23:34:52 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.svg"
+import "../styles/_Header.scss"
+
+function Header() {
+    return (
+        <header className="header">  
+            <NavLink to="/"><img src={logo} className="header__logo" alt="logo principal kasa"/></NavLink>
+            <nav>
+                <ul className="header__nav">
+                    <NavLink className="header__nav--link" to="/"><li>Accueil</li></NavLink>
+                    <NavLink className="header__nav--link" to="/apropos"><li>A propos</li></NavLink>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
